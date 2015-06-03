5aae10ba29c22490fc154fb4464f12de8a08bbf9
Mike Solomon <mike@mikesolomon.org>
2015-06-03 10:28:52 +0300

    Sets up infrastructure for preloading images

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-preload-image/angular-preload-image.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
