325c48c555b8a4d15667d17cc9edce176a2043b7
Mike Solomon <mike@mikesolomon.org>
2015-06-10 21:36:02 +0300

    New news

--- app/scripts/constants/news.js
+++ app/scripts/constants/news.js
@@ -3,38 +3,10 @@
 angular.module('tenorApp')
   .constant('news', [
     {
-      big:'Please fill me in',
-      small:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum elit mauris, lobortis convallis consequat ut, sodales quis orci.',
+      big:'The TENOR website goes live',
+      small:'Welcome to the TENOR website.',
       person: 'TENOR official',
-      postlink: 'please-fill-me-in',
-      date:'May 24, 2015'
-    },
-    {
-      big:'Here\'s some more news',
-      small:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar scelerisque orci, accumsan auctor elit sollicitudin et.',
-      person:'Dominique Fober',
-      postlink: 'more-info',
-      date:'April 25, 2015'
-    },
-    {
-      big:'You get the idea',
-      small:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada ipsum at pharetra mattis. Nunc vulputate et lectus cursus euismod.',
-      person:'Jean Bresson',
-      postlink: 'get-the-idea',
-      date:'March 25, 2015'
-    },
-    {
-      big:'One more for testing',
-      small:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada ipsum at pharetra mattis. Nunc vulputate et lectus cursus euismod.',
-      person:'Mike Solomon',
-      postlink: 'one-more',
-      date:'March 1, 2015'
-    },
-    {
-      big:'Last one',
-      small:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada ipsum at pharetra mattis. Nunc vulputate et lectus cursus euismod.',
-      person:'Mike Solomon',
-      postlink: 'last-one',
-      date:'March 1, 2015'
+      postlink: 'tenor-website-up',
+      date:'June 10, 2015'
     }
   ]);
