128bfd00ab851f5e4c4d3c083b437e15a8d93768
D. Fober <fober@grame.fr>
2015-06-11 16:27:05 +0200

    put people in alphabetical order

--- app/scripts/constants/people.js
+++ app/scripts/constants/people.js
@@ -5,26 +5,12 @@ angular.module('tenorApp')
       'TENOR official' : {
         website: 'http://www.tenor-conference.org'
       },
-      'Dominique Fober' : {
-        website: 'http://www.grame.fr',
-        steering: true,
-        affiliation: 'GRAME',
-        img : 'http://www.placehold.it/200x200',
-        blurb : 'Dominique Fober knows the last digit of pi.'
-      },
       'Jean Bresson' : {
         website : 'http://www.ircam.fr',
         steering: true,
         affiliation: 'IRCAM-CNRS UMR STMS',
         img : 'http://www.placehold.it/200x200',
         blurb : 'Every time Jean Bresson goes through a blood test, he gets an A+.'
-      },
-      'Richard Hoadley' : {
-        website : 'http://www.ircam.fr',
-        steering: true,
-        affiliation: 'Anglia Ruskin University',
-        img : 'http://www.placehold.it/200x200',
-        blurb : 'Richard Hoadley gets offered valet parking at Drive-Thrus.'
       },
       'Pierre Couprie' : {
         website : 'http://www.ircam.fr',
@@ -33,12 +19,26 @@ angular.module('tenorApp')
         img : 'http://www.placehold.it/200x200',
         blurb : 'Lightning is too slow to strick Pierre Couprie.'
       },
+      'Dominique Fober' : {
+        website: 'http://www.grame.fr',
+        steering: true,
+        affiliation: 'GRAME',
+        img : 'http://www.placehold.it/200x200',
+        blurb : 'Dominique Fober knows the last digit of pi.'
+      },
       'Yann Geslin' : {
         website : 'http://www.ircam.fr',
         steering: true,
         affiliation: 'INA-GRM',
         img : 'http://www.placehold.it/200x200',
         blurb : 'Yann Geslin hitchhiked to the moon and walked at least half the way.'
+      },
+      'Richard Hoadley' : {
+        website : 'http://www.ircam.fr',
+        steering: true,
+        affiliation: 'Anglia Ruskin University',
+        img : 'http://www.placehold.it/200x200',
+        blurb : 'Richard Hoadley gets offered valet parking at Drive-Thrus.'
       },
       'Mike Solomon' : {
         website: 'http://www.ensemble101.fr',
