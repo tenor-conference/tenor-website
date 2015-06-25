2458cbd98dc25d40e4676339593db75576741e03
Mike Solomon <mike@mikesolomon.org>
2015-06-25 13:19:24 +0300

    Allows abstracts and bibtex to appear separately

--- app/scripts/controllers/proceedings.js
+++ app/scripts/controllers/proceedings.js
@@ -11,4 +11,22 @@
 angular.module('tenorApp')
   .controller('ProceedingsCtrl', function ($scope, papers2015) {
     $scope.papers2015 = papers2015;
+    $scope.showing = [];
+    for (var i=0; i < papers2015.length; i++) {
+      $scope.showing.push('NOTHING');
+    }
+    $scope.toAbstract = function(idx) {
+      if ($scope.showing[idx] === 'ABSTRACT') {
+        $scope.showing[idx] = 'NOTHING';
+      } else {
+        $scope.showing[idx] = 'ABSTRACT';
+      }
+    };
+    $scope.toBibtex = function(idx) {
+      if ($scope.showing[idx] === 'BIBTEX') {
+        $scope.showing[idx] = 'NOTHING';
+      } else {
+        $scope.showing[idx] = 'BIBTEX';
+      }
+    };
   });
