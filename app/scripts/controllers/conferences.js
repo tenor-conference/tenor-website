0abc8179261d8d6358dea68270b4d30d4d728a5a
D. Fober <fober@grame.fr>
2015-07-06 17:56:45 +0200

    bibtex info added to 2015

--- app/scripts/controllers/conferences.js
+++ app/scripts/controllers/conferences.js
@@ -24,7 +24,12 @@ angular.module('tenorApp')
         date : 'May 28-30',
         cwebsite:'http://tenor2015.tenor-conference.org',
         pwebsite:'http://tenor2015.tenor-conference.org/TENOR2015-Proceedings.pdf',
-        bibtex:'bibtex2015.bib'
+        bibtex:'bibtex2015.bib',
+		address: 'Paris, France',
+		title: 'Proceedings of the First International Conference on Technologies for Music Notation and Representation - TENOR2015',
+		editor: 'Marc Battier and Jean Bresson and Pierre Couprie and Cécile Davy-Rigaux and Dominique Fober and Yann Geslin and Hugues Genevois and François Picard and Alice Tacaille',
+		publisher: 'Institut de Recherche en Musicologie',
+		ISBN: '978-2-9552905-0-7'
       }
     ];
   });
